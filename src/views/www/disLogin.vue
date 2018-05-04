63ddb0067f3db82333269d72e70a04809f3e2c80
Lwj1378384589 <1378384589@qq.com>
2018-05-04 10:20:39 +0800

    登陆

--- src/views/www/disLogin.vue
+++ src/views/www/disLogin.vue
@@ -1,19 +1,69 @@
 <template>
-    <div>
     <div class="reg" style=" padding: 40px 0 0 0;min-width: 1200px;" >
         <div class="css-main-u">
-            <div class="css-middle-banner">
-                <div class="css-container">
-                    <div class="css-middle-page">
-                        <router-link to="/www/StudentLogin"class="css-link" style="color: #ffffff;">学生登录</router-link>
-                        <router-link to="/www/unitLogin"class="css-link" style="color: #ffffff;">企业登录</router-link>
+            <div class="login-backgroundimg">
+                <!-- <div class="">
+                    <router-link to="/www/StudentLogin"class="css-link" style="color: #ffffff;">学生登录</router-link>
+                    <router-link to="/www/unitLogin"class="css-link" style="color: #ffffff;">企业登录</router-link>
+                </div> -->
+                <div class="login-contain">
+                    <div class="classify">
+                        <div  class="classify1"><router-link to="/www/unitLogin">企业登录</router-link></div>
+                        <div  class="classify2" style="color: #1296db;">学生登录</div>
+                    </div>
+                    <form action="" id="loginForm"  >
+                            <el-input v-model="username" style="width:270px;" placeholder="请输入学号" class="login-text" type="text"></el-input>                        
+                            <el-input v-model="password" style="width:270px;"   placeholder="请输入密码" class="login-pass" type="password"></el-input>
+                        <!-- <a href="ForgetPassword.html"  > 密码忘记了？<span style="color:#00abd8;">立即找回</span></a> -->
+                        <div class="login-passmore"> 
+                            <div class="zidong"><input type="checkbox" value="自动登录">自动登录</div>
+                            <div class="forget">忘记密码？</div>
+                        </div>
+                        <div class="login-btn">
+                        <a  id="showTooltips" @click="login">登录</a>
+                        </div>
+                    </form>
+                    <div class="login-sign">
+                        <p>立即注册</p>
                     </div>
                 </div>
+                
             </div>
         </div>
     </div>
-</div>
 </template>
-<script>
 
+<script>
+import axiosApi from "@/api/public"
+export default{
+    data() {
+        return {
+            username:'',
+            password:'',
+        }
+    },
+    methods:{
+        login(){
+            if(this.username===''){
+               alert("请输入学号！", "text");
+               return false;
+            }else if(this.password===''){
+                alert("请输入密码！", "text");
+                return false;
+            }else{
+                var id="5a741e52ddccf17e5562d188"
+                axiosApi.axiosPost('/apis/platform/user/member/passwd?_id='+id,
+                {
+                    "username": this.username,
+                    "password": this.password
+                }).then(function(res){
+                    alert(res.data)
+                })
+            }
+        }
+    }
+}
 </script>
+
+<style scoped>
+</style>
--- src/views/www/unitLogin.vue
+++ src/views/www/unitLogin.vue
@@ -1,43 +1,31 @@
 <template>
-        <div class="reg" style="padding-top: 40px;">
-				<div class="css-main-u">
-					<div class="css-log-banner">
-						<div class="css-container">
-                <div class="css-text"> <!-- class="css-text" -->
-								<p class="css-p1">
-									慧安&nbsp;&nbsp;(HuiAn)
-								</p>
-								<p class="css-p1" style="padding-top: 0;">
-									智慧就业云平台
-								</p>
-								<p class="css-p2">
-									在这里
-								</p>
-								<p class="css-p2">
-									连接
-								</p>
-								<p class="css-p2">
-									职场精英
-								</p>
-							</div>
-							<div class="logConbox">
-								<div class="logTitle">企业登录</div>
-								<form action="" id="loginForm"  class="logForm">
-									<div class="logInput">
-										<div class="logTit">单位全称</div>
-										<el-input v-model="entName" placeholder="单位全称" class="logGroup" type="text"></el-input>
-									</div>
-									<div class="logInput">
-										<div class="logTit">登录密码</div>
-										<el-input v-model="entPassword" placeholder="登录密码" class="logGroup" type="password" @keyup.enter.native="login"></el-input>
-									</div>
-									<a class="logBtn" href="javascript:" id="showTooltips" @click.keyup.13="login">登录</a>
-								</form>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+  <div class="reg" style="padding-top: 40px;">
+    <div class="css-main-u">
+      <div class="login-backgroundimg">
+        <div class="login-contain">
+          <div class="classify">
+            <div  class="classify1" style="color: #1296db;">企业登录</div>
+            <div  class="classify2"><router-link to="/www/disLogin">学生登录</router-link></div>
+          </div>
+          <form action="" id="loginForm" >
+            <el-input v-model="entName" style="width:270px;"  class="login-text" placeholder="单位全称"  type="text"></el-input>
+            <el-input v-model="entPassword" style="width:270px;"  placeholder="登录密码" class="login-pass" type="password" @keyup.enter.native="login"></el-input>
+            <div class="login-passmore"> 
+              <div class="zidong"><input type="checkbox" value="自动登录">自动登录</div>
+              <div class="forget">忘记密码？</div>
+            </div>
+            <div class="login-btn">
+            <a href="javascript:" id="showTooltips" @click.keyup.13="login">登录</a>
+            <!-- <input type="button" value="登录" class="login-btn"/> -->
+            </div>
+          </form>
+          <div class="login-sign">
+            <p>立即注册</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import axiosApi from "@/api/public"
@@ -77,8 +65,7 @@ export default{
                 alert("您的账号未通过审核");
                 /* window.location.href="/ent/login"; */
               }else if(response.data.data.status=="0"){
-                 window.location.href="backPage.html"
-                // _this.$router.push({path:'/backpage'})
+                  window.location.href="backPage.html"                // _this.$router.push({path:'/backpage'})
                } 
               // else{
               //   alert("您还没有注册,请注册");
@@ -98,3 +85,6 @@ export default{
     }
   }
 </script>
+
+<style scoped>
+</style>
